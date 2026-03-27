1ef274deff966e78a1c904f7e169330397887558
DevarathN <devarathnediyirippil@gmail.com>
2026-03-27 23:03:16 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,30 @@
 export const api = {
-  register: (data) => {
+  register: ({ name, email, password }) => {
     const users = JSON.parse(localStorage.getItem("users") || "[]");
 
-    // prevent duplicate users
-    const exists = users.find((u) => u.email === data.email);
+    const exists = users.find((u) => u.email === email);
     if (exists) {
       alert("User already exists");
       return;
     }
 
-    users.push(data);
+    const newUser = {
+      name,
+      email,
+      password,
+      role: email === "admin@gmail.com" ? "admin" : "user", 
+      balance: 0,
+    };
+
+    users.push(newUser);
     localStorage.setItem("users", JSON.stringify(users));
   },
 
   login: ({ email, password }) => {
     const users = JSON.parse(localStorage.getItem("users") || "[]");
 
-    return users.find((u) => u.email === email && u.password === password);
+    return users.find(
+      (u) => u.email === email && u.password === password
+    );
   },
 };
